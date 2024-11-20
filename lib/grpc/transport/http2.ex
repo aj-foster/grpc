f130299bdb9b77cd77d0d67c603507b59d09dac2
AJ Foster <2789166+aj-foster@users.noreply.github.com>
2024-11-20 10:43:33 -0500

    Resolve map.field notation warning during tests

--- lib/grpc/transport/http2.ex
+++ lib/grpc/transport/http2.ex
@@ -70,6 +70,7 @@ defmodule GRPC.Transport.HTTP2 do
   # to avoid this kind of trouble, use application/grpc by default
   defp content_type(_, GRPC.Codec.Proto), do: "application/grpc"
   defp content_type(_, codec = GRPC.Codec.WebText), do: "application/grpc-web-#{codec.name()}"
+  defp content_type(_, %{} = codec), do: "application/grpc+#{codec.name}"
   defp content_type(_, codec), do: "application/grpc+#{codec.name()}"
 
   def extract_metadata(headers) do
